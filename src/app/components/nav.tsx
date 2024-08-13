a37460cf6ce7040e7aef6bc3112d3929b7cdc410
Daniel Jimenez <dannyyy.jimenez@gmail.com>
2024-08-13 12:42:24 -0500

    first commit

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { GrCatalog } from "react-icons/gr";
+import { MdEmail } from "react-icons/md";
+import { PiMailboxFill, PiPhoneFill } from "react-icons/pi";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+const DynamicLottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+export default function Nav() {
+  return (
+    <>
+      <div className="fixed w-full z-20 max-sm:px-4 px-16 py-4 flex flex-row content-center items-center h-32">
+        <Image
+          className="w-auto inline-flex max-sm:w-20"
+          height={72}
+          width={150}
+          src="https://res.cloudinary.com/labodegaltd/image/upload/v1723494448/quesosgallito/logotipomenu_333x227.png"
+        />
+        <div className="flex-auto" />
+        <p className="max-sm:hidden w-auto text-white flex items-center">
+          <span className="relative h-[32px] w-[32px] flex items-center justify-center mr-2">
+            <Image
+              className="absolute top-0"
+              width={32}
+              height={32}
+              src="https://res.cloudinary.com/labodegaltd/image/upload/v1723496587/quesosgallito/cheese-icon-holder-a.png"
+            />
+            <PiPhoneFill className="z-10 text-xl text-[#0066b1]" />
+          </span>
+          (312) 243-2755
+        </p>
+        <div className="flex-auto" />
+        <p className="max-sm:hidden w-auto text-white flex items-center">
+          <span className="relative h-[32px] w-[32px] flex items-center justify-center mr-2">
+            <Image
+              className="absolute top-0"
+              width={32}
+              height={32}
+              src="https://res.cloudinary.com/labodegaltd/image/upload/quesosgallito/cheese-icon-holder-b.png"
+            />
+            <MdEmail className="z-10 text-xl text-[#0066b1]" />
+          </span>
+          info@quesoselgallito.com
+        </p>
+        <div className="flex-auto" />
+        <div className="flex-auto" />
+        <Image
+          className="transform  max-sm:scale-75 max-sm:translate-y-0 scale-150 translate-y-5"
+          width={150}
+          height={150}
+          src="https://res.cloudinary.com/labodegaltd/image/upload/v1723565362/quesosgallito/available-hacienda-retailers.png"
+        />
+      </div>
+      <button className="fixed bg-white z-20 shadow-[rgba(255,255,255,0.3)] shadow-xl px-6 py-4 rounded-full flex items-center bottom-10 right-32">
+        <p>View Product Catalog</p>
+        <GrCatalog className="ml-2" />
+        <Link
+          target="_blank"
+          href="https://issuu.com/labodegaltd/docs/elgallito-cheese-dairy-2024-digital-catalog?fr=xGAEgASgBPf8zNelCO0OJWgZqOlgDGPTBQDvIdVBhwSY7FgbhCsH_BDIwMjLB_wIwNMH-TAT-QgZqOgT_Al9MBjoGyP4xbjtU"
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      </button>
+      <div className="fixed flex z-20 justify-center bottom-4 right-12">
+        <DynamicLottie
+          animationData={require("../assets/UDm86U069p.json")}
+          className="flex justify-center items-center w-8"
+          loop={true}
+        />
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,130 @@
 import Image from "next/image";
+import Nav from "./components/nav";
+import { M_PLUS_Rounded_1c } from "next/font/google";
+import {
+  BiLogoFacebook,
+  BiLogoFacebookSquare,
+  BiLogoInstagram,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import Link from "next/link";
+import { SiInstacart } from "react-icons/si";
+
+const mPlusRounded = M_PLUS_Rounded_1c({ subsets: ["latin"], weight: "700" });
 
 export default function Home() {
+  const products = [
+    {
+      id: "crema-agria",
+      color: "#0066b1",
+      title: "100% Natural 🥛",
+    },
+    {
+      id: "quesadilla-rayado",
+      color: "#f48331",
+      title: "Mexican Style 🇲🇽",
+    },
+    {
+      id: "queso-panela",
+      color: "#23266b",
+      title: "Part-Skim Cheese 🧀",
+    },
+    {
+      id: "queso-oaxaca",
+      color: "#e91524",
+      title: "Melting Cheese 🫕🧀",
+    },
+    {
+      id: "queso-munster",
+      color: "#3460a9",
+      title: "Soft & Tasty 😋",
+    },
+    {
+      id: "cotija-polvo",
+      color: "#0ea64f",
+      title: "Grated Cheese 🧀🌀",
+    },
+    {
+      id: "queso-fresco",
+      color: "#0186e1",
+      title: "Crumbling Cheese 🧀🔨",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
+      <Nav />
+      {products.map((product) => (
+        <main
+          key={product.id}
+          style={{
+            backgroundColor: product.color,
+          }}
+          className="flex max-md:p-10 snap-center snap-always min-h-screen relative flex-col items-center justify-between p-24"
+        >
+          <Image
+            width={1000}
+            height={1000}
+            className="max-sm:h-[80vh] object-contain"
+            src={`https://res.cloudinary.com/labodegaltd/image/upload/q_auto,f_auto/v12423443/quesosgallito/${product.id}.png`}
+          />
+          <h1
+            className={
+              "font-bold max-sm:text-[32px] max-sm:bottom-32 max-sm:left-0 max-sm:w-full max-sm:text-center absolute bottom-2 left-10 text-[72px] text-white " +
+              mPlusRounded.className
+            }
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {product.title}
+          </h1>
+        </main>
+      ))}
+
+      <div className="fixed max-sm:hidden top-80 right-14 flex flex-col">
+        <Link
+          target="_blank"
+          href="https://www.facebook.com/quesoselgallito"
+          className={
+            "text-lg my-2 text-white flex row items-center opacity-80 " +
+            mPlusRounded.className
+          }
+        >
+          <BiLogoFacebookSquare style={{ height: 30, width: 30 }} />
+          &nbsp;&nbsp;quesoselgallito
+        </Link>
+        <Link
+          target="_blank"
+          href="https://www.youtube.com/channel/UC9OvJ2Z9uhV8y8P4SVO4D_g"
+          className={
+            "text-lg my-2 text-white flex row items-center opacity-80 " +
+            mPlusRounded.className
+          }
+        >
+          <BiLogoYoutube style={{ height: 30, width: 30 }} />
+          &nbsp;&nbsp;Quesos El Gallito
+        </Link>
+        <Link
+          target="_blank"
+          href="https://www.instagram.com/quesoselgallito/?hl=en"
+          className={
+            "text-lg my-2 text-white flex row items-center opacity-80 " +
+            mPlusRounded.className
+          }
+        >
+          <BiLogoInstagram style={{ height: 30, width: 30 }} />
+          &nbsp;&nbsp;quesoselgallito
+        </Link>
+        <Link
+          target="_blank"
+          href="https://www.instacart.com/store/s?k=el+gallito"
+          className={
+            "text-lg my-2 text-white flex row items-center opacity-80 " +
+            mPlusRounded.className
+          }
+        >
+          <SiInstacart style={{ height: 30, width: 30 }} />
+          &nbsp;&nbsp;quesoselgallito
+        </Link>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
